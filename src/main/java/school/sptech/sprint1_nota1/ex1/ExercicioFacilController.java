94f827ef3a88d44b5b1beaaec8e4bf6fed4c3ff2
LucasDev-end <lucas.marcolino@sptech.school>
2025-08-19 12:41:11 -0300

    Atividade concluida

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,21 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+        palavra = palavra.toLowerCase();
+        String palindromo = new StringBuilder(palavra).reverse().toString();
+
+        if (palavra.equals(palindromo)){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -1,14 +1,45 @@
 package school.sptech.sprint1_nota1.ex3;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) throws JsonProcessingException {
+        ExercicioDificilResponse resposta = new ExercicioDificilResponse();
+
+        if (n <= 0){
+            resposta.enesimoTermo = 0;
+            resposta.soma = 0;
+        }else {
+
+            int anterior = 0;
+            int atual = 1;
+            resposta.soma = anterior + atual;
+
+            if (n == 1) {
+                resposta.enesimoTermo = 1;
+                resposta.soma = 1;
+            }else {
+
+                for (int i = 2; i <= n; i++) {
+                    int aux = anterior + atual;
+                    resposta.soma += aux;
+                    anterior = atual;
+                    atual = aux;
+                }
+            }
+            resposta.enesimoTermo = atual;
+        }
+        return resposta;
     }
 }
